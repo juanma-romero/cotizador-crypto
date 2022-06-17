6bc040700203c36f17efc9249158beb3f6b84d4a
Juanma Romero <xjuanma.romerox@gmail.com>
2022-06-16 20:51:36 -0400

    Set Up, hasta cap 134

---
+++ cotizador-crypto/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #fff
+`
+
+function App() {
+  
+
+  return (
+    <Heading>hola</Heading>
+  )
+}
+
+export default App
