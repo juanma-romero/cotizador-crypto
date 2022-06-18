fd5810b3ecfa6ef0bd4314888435570f61befd38
Juanma Romero <xjuanma.romerox@gmail.com>
2022-06-17 21:16:35 -0400

    estilos y componente Formulario

--- cotizador-crypto/src/App.jsx
+++ cotizador-crypto/src/App.jsx
@@ -1,16 +1,59 @@
 import { useState } from 'react'
 import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './componentes/Formulario'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+
+`
 
 const Heading = styled.h1`
   font-family: 'Lato', sans-serif;
-  color: #fff
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
 `
 
 function App() {
   
 
   return (
-    <Heading>hola</Heading>
+    <Contenedor>
+      <Imagen 
+        src={ImagenCripto} 
+        alt='imagenes criptomonedas' 
+      />
+      <div>
+      <Heading>Cotiza criptomonedas al instante</Heading>
+      <Formulario />
+      </div> 
+    </Contenedor>
   )
 }
 
---
+++ cotizador-crypto/src/componentes/Formulario.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const InputSubmit = styled.input`
+    background-color: #9497FF;
+    border: none;
+    width: 100%;
+    padding: 10px;
+    color: #FFF;
+    font-weight: 700;
+    text-transform: uppercase;
+    font-size: 20px;
+    border-radius: 5px;
+    transition: background-color .3s ease;
+    
+
+    &:hover {
+        background-color: #7a7dfe;
+        cursor: pointer;
+    }
+`
+
+const Formulario = () => {
+  return (
+    <form>
+
+        <InputSubmit type='submit' value='Cotizar' />
+    </form>
+  )
+}
+
+export default Formulario
